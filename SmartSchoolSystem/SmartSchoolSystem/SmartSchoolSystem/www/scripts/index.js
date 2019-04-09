f70fc9e4a1e53206e392b86efad0993c5584b333
DanielRiko <DanielRiko@users.noreply.github.com>
2019-04-09 17:57:04 +0400

    Alpha 0.0.1
    I uploadedd the source code of the application!

---
+++ SmartSchoolSystem/SmartSchoolSystem/SmartSchoolSystem/www/scripts/index.js
@@ -0,0 +1,45 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkID=397704
+// To debug code on page load in cordova-simulate or on Android devices/emulators: launch your app, set breakpoints, 
+// and then run "window.location.reload()" in the JavaScript Console.
+(function () {
+    "use strict";
+
+    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+
+    function onDeviceReady() {
+        // Handle the Cordova pause and resume events
+        document.addEventListener( 'pause', onPause.bind( this ), false );
+        document.addEventListener( 'resume', onResume.bind( this ), false );
+        
+        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
+        var parentElement = document.getElementById('deviceready');
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+    };
+
+    document.getElementById("followers_link").addEventListener("click", sendToNotifications);
+    document.getElementById("Group_3").addEventListener("click", sendToReminder);
+    document.getElementById("reviews_link").addEventListener("click", sendToMain);
+
+    function sendToNotifications() {
+        location.replace("Notification.html");
+    }
+
+    function sendToMain() {
+        location.replace("index.html");
+    }
+
+    function sendToReminder() {
+        location.replace("Reminder.html");
+    }
+    function onPause() {
+        // TODO: This application has been suspended. Save application state here.
+    };
+
+    function onResume() {
+        // TODO: This application has been reactivated. Restore application state here.
+    };
+} )();
